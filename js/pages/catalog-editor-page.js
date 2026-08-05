faf03e30185dd6b2342aff983569a5c36c6bdb33
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-05 22:37:21 +0300

    stage 2: add js/pages/catalog-editor-page.js

---
+++ js/pages/catalog-editor-page.js
@@ -0,0 +1,7 @@
+import { initializePage } from '../app.js';
+
+initializePage({
+  title: 'Редактор каталога',
+  activeNav: 'admin-catalog',
+  content: 'Бизнес-функции этой страницы будут реализованы на отдельном этапе.'
+});
