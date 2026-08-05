092e75f1a908114c3e156ff379e4076476219ddb
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-05 22:37:20 +0300

    stage 2: add js/pages/profile-page.js

---
+++ js/pages/profile-page.js
@@ -0,0 +1,7 @@
+import { initializePage } from '../app.js';
+
+initializePage({
+  title: 'Профиль',
+  activeNav: 'profile',
+  content: 'Бизнес-функции этой страницы будут реализованы на отдельном этапе.'
+});
