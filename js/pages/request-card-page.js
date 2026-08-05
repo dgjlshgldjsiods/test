17de504fe9adf47e42a122ad82a0e9e1d7710997
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-05 23:08:49 +0300

    stage 5: update js/pages/request-card-page.js

--- js/pages/request-card-page.js
+++ js/pages/request-card-page.js
@@ -1,7 +1,7 @@
 import { initializeProtectedPage } from '../app.js';
 
 await initializeProtectedPage({
-  title: 'Карточка заявки',
+  titleKey: 'pages.requestCard',
   activeNav: 'requests',
-  content: 'Бизнес-функции этой страницы будут реализованы на отдельном этапе.'
+  contentKey: 'pages.stub'
 });
