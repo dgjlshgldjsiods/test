85ce842856f15f146041eca3070cf3566fd0bdda
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-05 23:08:39 +0300

    stage 5: update js/pages/catalog-editor-page.js

--- js/pages/catalog-editor-page.js
+++ js/pages/catalog-editor-page.js
@@ -2,8 +2,8 @@ import { initializeProtectedPage } from '../app.js';
 import { PAGE_ROLES } from '../core/permissions.js';
 
 await initializeProtectedPage({
-  title: 'Редактор каталога',
+  titleKey: 'pages.catalogEditor',
   activeNav: 'admin-catalog',
   requiredRoles: PAGE_ROLES.catalogEditor,
-  content: 'Бизнес-функции этой страницы будут реализованы на отдельном этапе.'
+  contentKey: 'pages.stub'
 });
