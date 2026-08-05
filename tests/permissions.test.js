507c5978ad309fb6b619d163614ee1acaa1c0594
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-05 22:59:21 +0300

    stage 4: add tests/permissions.test.js

---
+++ tests/permissions.test.js
@@ -0,0 +1,31 @@
+import { PAGE_ROLES, ROLES, canAccessAdminSection, hasAnyRole, hasRole, normalizeRoles } from '../js/core/permissions.js';
+
+QUnit.module('Permissions', () => {
+  QUnit.test('нормализует и удаляет дубликаты ролей', (assert) => {
+    assert.deepEqual(normalizeRoles([' USER ', 'USER', null, '']), ['USER']);
+    assert.deepEqual(normalizeRoles(null), []);
+  });
+
+  QUnit.test('проверяет точную роль', (assert) => {
+    const user = { roles: [ROLES.USER, ROLES.OPERATOR] };
+    assert.true(hasRole(user, ROLES.OPERATOR));
+    assert.false(hasRole(user, ROLES.FORM_ADMIN));
+  });
+
+  QUnit.test('SYSTEM_ADMIN наследует все роли', (assert) => {
+    const user = { roles: [ROLES.SYSTEM_ADMIN] };
+    assert.true(hasRole(user, ROLES.CATALOG_ADMIN));
+    assert.true(hasAnyRole(user, PAGE_ROLES.slaEditor));
+  });
+
+  QUnit.test('пустой список ролей разрешает обычную защищённую страницу', (assert) => {
+    assert.true(hasAnyRole({ roles: [ROLES.USER] }, []));
+  });
+
+  QUnit.test('разделы администратора изолированы', (assert) => {
+    const catalogAdmin = { roles: [ROLES.CATALOG_ADMIN] };
+    assert.true(canAccessAdminSection(catalogAdmin, 'catalogEditor'));
+    assert.false(canAccessAdminSection(catalogAdmin, 'forms'));
+    assert.false(canAccessAdminSection(catalogAdmin, 'slaEditor'));
+  });
+});
