2ee793f740ca6932e341eb8aa25cc2bb1f24c10a
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-05 22:37:24 +0300

    stage 2: add js/pages/sla-editor-page.js

---
+++ js/pages/sla-editor-page.js
@@ -0,0 +1,7 @@
+import { initializePage } from '../app.js';
+
+initializePage({
+  title: 'Настройка SLA',
+  activeNav: 'admin-sla',
+  content: 'Бизнес-функции этой страницы будут реализованы на отдельном этапе.'
+});
