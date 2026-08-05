82678a80b1bdb36cc446ec6ddbb1b3106a5aece6
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-05 23:08:51 +0300

    stage 5: update js/pages/requests-page.js

--- js/pages/requests-page.js
+++ js/pages/requests-page.js
@@ -1,7 +1,7 @@
 import { initializeProtectedPage } from '../app.js';
 
 await initializeProtectedPage({
-  title: 'Заявки',
+  titleKey: 'pages.requests',
   activeNav: 'requests',
-  content: 'Бизнес-функции этой страницы будут реализованы на отдельном этапе.'
+  contentKey: 'pages.stub'
 });
