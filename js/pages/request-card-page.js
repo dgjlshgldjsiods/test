3a95c2d20f2ec1be0bf2b6f8a026b43b0f3d5f99
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-05 22:37:19 +0300

    stage 2: add js/pages/request-card-page.js

---
+++ js/pages/request-card-page.js
@@ -0,0 +1,7 @@
+import { initializePage } from '../app.js';
+
+initializePage({
+  title: 'Карточка заявки',
+  activeNav: 'requests',
+  content: 'Бизнес-функции этой страницы будут реализованы на отдельном этапе.'
+});
