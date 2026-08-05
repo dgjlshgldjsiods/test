d6301c9bdcab3a945566c5fc012e83a426ad5db5
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-05 22:37:05 +0300

    stage 2: add js/config.js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+window.ITSM_CONFIG = Object.freeze({
+  baseUrl: 'https://replace-with-naumen-address',
+  accessKey: 'replace-with-access-key',
+  moduleName: 'modules.newItsmTest',
+  requestTimeout: 30000,
+  debug: false,
+  defaultLanguage: 'ru',
+  defaultTheme: 'light'
+});
