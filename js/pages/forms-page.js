a04d2f3652f111e0d3f21a6ea1562f92ece52fcd
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-05 22:37:22 +0300

    stage 2: add js/pages/forms-page.js

---
+++ js/pages/forms-page.js
@@ -0,0 +1,7 @@
+import { initializePage } from '../app.js';
+
+initializePage({
+  title: 'Формы',
+  activeNav: 'admin-forms',
+  content: 'Бизнес-функции этой страницы будут реализованы на отдельном этапе.'
+});
