63de8e3763a289e1f57af4e6585431f0709c94d4
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-05 22:37:17 +0300

    stage 2: add js/pages/service-form-page.js

---
+++ js/pages/service-form-page.js
@@ -0,0 +1,7 @@
+import { initializePage } from '../app.js';
+
+initializePage({
+  title: 'Создание заявки',
+  activeNav: 'catalog',
+  content: 'Бизнес-функции этой страницы будут реализованы на отдельном этапе.'
+});
