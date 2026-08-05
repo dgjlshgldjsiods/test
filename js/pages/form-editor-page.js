42d59f8445d606fcfcf4b4b66ed0d4e3812c9bb1
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-05 22:37:23 +0300

    stage 2: add js/pages/form-editor-page.js

---
+++ js/pages/form-editor-page.js
@@ -0,0 +1,7 @@
+import { initializePage } from '../app.js';
+
+initializePage({
+  title: 'Редактор формы',
+  activeNav: 'admin-forms',
+  content: 'Бизнес-функции этой страницы будут реализованы на отдельном этапе.'
+});
