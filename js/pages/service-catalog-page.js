ba9353297354342cf8c863285459fea1e3bde5a4
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-05 23:08:53 +0300

    stage 5: update js/pages/service-catalog-page.js

--- js/pages/service-catalog-page.js
+++ js/pages/service-catalog-page.js
@@ -1,7 +1,7 @@
 import { initializeProtectedPage } from '../app.js';
 
 await initializeProtectedPage({
-  title: 'Каталог услуг',
+  titleKey: 'pages.serviceCatalog',
   activeNav: 'catalog',
-  content: 'Бизнес-функции этой страницы будут реализованы на отдельном этапе.'
+  contentKey: 'pages.stub'
 });
