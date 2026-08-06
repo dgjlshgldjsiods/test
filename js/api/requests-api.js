596c49e931470c158175515f28a79904f0d0a524
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-06 11:33:26 +0300

    Implement request creation form

---
+++ js/api/requests-api.js
@@ -0,0 +1,9 @@
+export class RequestsApi {
+  constructor(apiClient) { this.apiClient = apiClient; }
+
+  create({ serviceId, formId, formVersionId, fieldValues, attachmentIds = [] }) {
+    return this.apiClient.exec('requestsCreate', {
+      serviceId, formId, formVersionId, fieldValues, attachmentIds
+    });
+  }
+}
