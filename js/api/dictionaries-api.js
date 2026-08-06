f57bcc48058284ca20b47fa019f8d9743554c0ec
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-06 11:08:42 +0300

    Implement catalog editor

---
+++ js/api/dictionaries-api.js
@@ -0,0 +1,8 @@
+export class DictionariesApi {
+  constructor(apiClient) { this.apiClient = apiClient; }
+  getGroups(request = {}) { return this.apiClient.exec('dictionariesGetGroups', request); }
+  getOrganizations(request = {}) { return this.apiClient.exec('dictionariesGetOrganizations', request); }
+  getDepartments(request = {}) { return this.apiClient.exec('dictionariesGetDepartments', request); }
+  getItems(dictionaryCode, request = {}) { return this.apiClient.exec('dictionariesGetItems', { ...request, dictionaryCode }); }
+  searchUsers(request = {}) { return this.apiClient.exec('dictionariesSearchUsers', request); }
+}
