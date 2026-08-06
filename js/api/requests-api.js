fc12b5b5a11646fb17c42c0c705b20d84340da7c
dgjlshgldjsiods <wm12-34@mail.ru>
2026-08-06 12:53:03 +0300

    Stage 12: implement request card

--- js/api/requests-api.js
+++ js/api/requests-api.js
@@ -10,4 +10,19 @@ export class RequestsApi {
   getList(request = {}) {
     return this.apiClient.exec('requestsGetList', request);
   }
+
+  get(entityId) { return this.apiClient.exec('requestsGet', { entityId }); }
+  changeStatus(entityId, newStatus, expectedVersion, comment = '') {
+    return this.apiClient.exec('requestsChangeStatus', { entityId, newStatus, comment, expectedVersion });
+  }
+  changeAssignment(entityId, responsibleGroupId, assigneeId, expectedVersion) {
+    return this.apiClient.exec('requestsChangeAssignment', { entityId, responsibleGroupId, assigneeId, expectedVersion });
+  }
+  addComment(entityId, type, text, expectedVersion) {
+    return this.apiClient.exec('requestsAddComment', { entityId, comment: { type, text }, expectedVersion });
+  }
+  getComments(entityId, request = {}) { return this.apiClient.exec('requestsGetComments', { ...request, entityId }); }
+  getHistory(entityId, request = {}) { return this.apiClient.exec('requestsGetHistory', { ...request, entityId }); }
+  getAttachments(entityId) { return this.apiClient.exec('requestsGetAttachments', { entityId }); }
+  getSla(entityId) { return this.apiClient.exec('requestsGetSla', { entityId }); }
 }
